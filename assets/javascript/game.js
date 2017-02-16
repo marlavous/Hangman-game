e5dc9ce35201249e69e8fa608af19643bedf04c9
Mar <marcianelli.m@att.net>
2017-02-15 19:45:16 -0800

    more work

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,41 +1,39 @@
-//window.onload = (function(){startGame()});
-
 var words = [
 	"whales",
 	"badger",
 	"hummingbird",
-	"flacon",
+	"falcon",
 	"jellyfish",
 	"sealion"
 	]
 
 var currentWord = "";
-var lettersInChosenWord = []; //turn array into a string
+var chosenLetters = []; //turn array into a string
 var numBlanks = 0;  //num of letters in array
-var blanksAndSuccesses = []; //will restart game auto when game is complete
+var levelUp = []; //will restart game auto when game is complete
 var wrongGuess = [];
 
 var wins = 0;
-var lossses = 0;
-var numGuesses = 13;
+var losses = 1;
+var numGuesses = 8;
 
 function startGame(){
-	var guesses = 13;
-	var blanksAndSuccesses = [];
+	var numGuesses = 8;
+	var levelUp = [];
 	var wrongGuess = [];
 
 	currentWord = words[Math.floor(Math.random() * words.length)]
-	lettersInChosenWord = currentWord.split("");
-	numBlanks = lettersInChosenWord.length;
+	chosenLetters = currentWord.split("");
+	numBlanks = chosenLetters.length;
 	console.log(currentWord);
 	console.log(numBlanks);
 
-	for(var i = 0; i < numBlanks; i++){
-			blanksAndSuccesses.push("_");
+	for(var i = 0; i < numBlanks; i++){    
+			levelUp.push("_");
 		}
-	console.log(blanksAndSuccesses);
+	console.log(levelUp);
 	document.getElementById("guesses-left").innerHTML = numGuesses;	
-	document.getElementById("word").innerHTML = blanksAndSuccesses.join("  ");
+	document.getElementById("word-blank").innerHTML = levelUp.join("  ");
 	}
 
 function checkLetters(letter){
@@ -43,16 +41,15 @@ function checkLetters(letter){
 
 	for (var i = 0; i < numBlanks; i++){
 		if(currentWord[i] === letter){
-			letterInWord = true;  //if its true run the loop
+			letterInWord = true;  //how do you check if te guess ixs in the word?
 		}
 	}
 
 	if(letterInWord){                            //how do you keep score if they guess right?
 		for(i = 0; i < numBlanks; i++){
 			if(currentWord[i] === letter){
-			blanksAndSuccesses[i] = letter;
+			levelUp[i] = letter;
 			}
-			//console.log("im inside ths right function", blanksAndSuccesses);
 			}
 		}
 	
@@ -60,12 +57,29 @@ function checkLetters(letter){
 		numGuesses --;
 		wrongGuess.push(letter);
 	}
-	//console.log("wrong guesses in this function", wrongGuess);
+	
 }
 
 function roundComplete(){  //update html here and determine if user wins
+	document.getElementById("word-blank").innerHTML = levelUp.join(" _ ");
+	document.getElementById("guesses-left").innerHTML = numGuesses;
+	
 
+	console.log(chosenLetters);
 
+	if(chosenLetters === levelUp){
+		wins++;
+		alert("You Win!!!");
+		document.getElementById("wincounter").innerHTML = wins;
+		startGame();
+	}
+	else if(numGuesses === 0){
+		document.getElementById("losscounter").innerHTML = losses++;
+		document.getElementById("wrong-guesses").innerHTML = " ";
+		alert("Sorry, you lost this round");
+		var wrongGuess = [];
+		startGame();
+	}
 }
 
 
@@ -73,8 +87,8 @@ startGame();
 
 document.onkeyup = function(event){
 	var letterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
-	//console.log("this is the letter typed", letterGuessed);
 	checkLetters(letterGuessed);
+	roundComplete();
 }
 
 
